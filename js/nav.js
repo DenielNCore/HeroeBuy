7af4e994cf5468699b6d027a67003d1706ee2612
DenielNCore <denielncore@gmail.com>
2018-06-22 20:06:00 +0300

    v1.1

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,14 @@
-let $div = $("<div>Hello world</div>");
+$(".toggleNav").click(() => {
+    let $ul = $('nav > ul');
+    let $nav = $(`.toggleNav`);
+    if($nav.offset().left===40) {
+        $ul.slideDown(500);
+        $nav.css({left: '2px'});
+        $nav.addClass("toggleNav2");
+    }
+    if($nav.offset().left===2) {
+        $ul.slideUp(500);
+        $nav.css({left: '40px'});
+        $nav.removeClass("toggleNav2");
+    }
+});
